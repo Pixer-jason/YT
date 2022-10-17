96e562471603d0f7a81c82b1884abfc4c34fc9ff
Pixer-jason <109621806+Pixer-jason@users.noreply.github.com>
2022-10-17 10:46:59 -0400

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,54 @@
+import os 
+import pytube
+#import tqdm
+#from pathlib import Path
+import time
+
+os.system("cls")
+def greetings():
+    print("""\033[1;34;40m
+██╗   ██╗████████╗    ██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗ ███████╗██████╗ 
+╚██╗ ██╔╝╚══██╔══╝    ██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+ ╚████╔╝    ██║       ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║█████╗  ██████╔╝
+  ╚██╔╝     ██║       ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗
+   ██║      ██║       ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║
+   ╚═╝      ╚═╝       ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+    """)
+    print("\033[1;31;40m\t\t\tDeveloped By Pixer Jason\n")
+    print("\033[1;33;40mInsipred By TripleHat\n")
+
+greetings()
+
+print("\033[1;37;40m1.Download 1080p Video\n")
+print("2.Download 720p Video\n")
+print("3.Change Dirrectory\n")
+ingiza = input("Select Your Option: " + "\n")
+
+for i in range (3):
+    print("Please wait... ", i)
+    time.sleep(1)
+    os.system("cls")
+    i=i+1
+
+if ingiza == '1':
+    lv = input("Enter YouTube URL For !080p: ")
+    time.sleep(3)
+    dwn = pytube.YouTube(lv)
+    print("Downloading..\../..\../\n")
+    bangi = dwn.streams.get_highest_resolution().download(print("Finalizing..........\n"))
+    print("Downloaded........... 100%", lv)
+    print(bangi + "\n")
+
+
+elif ingiza == '2':
+    lv = input("Enter YouTube URL For 720pp: ")
+    time.sleep(3)
+    dwn = pytube.YouTube(lv)
+    print("Downloading..\../..\../\n")
+    bangi = dwn.streams.get_highest_resolution().download(print("Finalizing..........\n"))
+    print("Downloaded........... 100%", lv)
+    print(bangi + "\n")
+
+elif ingiza == '3':
+    print("\033[1;31;40mOops! There's Nothing Up Here!\n")
+    exit
