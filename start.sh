9702dee5b2332fb4cacc74c1904b877d702cf8ff
Pixer-jason <109621806+Pixer-jason@users.noreply.github.com>
2022-10-20 09:31:23 -0400

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$EUID" == 0 ]
+  then echo "Please run Without Root"
+  exit
+fi
+
+if [[ -e "$PATH/python3" ]]
+then
+	echo -e "\033[9$(( RANDOM % 6 ))m"
+else
+	echo -e "\033[9$(( RANDOM % 6 ))m[!] Please Wait..."
+	apt install python -y > /dev/null
+fi
+
+python3 email.py
